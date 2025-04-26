d83db46af13264e115ba9d5b16502ad4ed963461
simranluthra <simran.19july@gmail.com>
2025-04-26 12:42:30 +0530

    Update gulpfile.js

--- starter/gulpfile.js
+++ starter/gulpfile.js
@@ -2,25 +2,23 @@ import gulp from 'gulp';
 import gulpShell from 'gulp-shell';
 import mocha from 'gulp-mocha';
 
-// Task to build and serve the project using Parcel
 gulp.task('parcel-build', gulpShell.task([
   'npx parcel build index.html --dist-dir dist --public-url ./',
 ]));
 
-// Task to serve the project using Parcel
 gulp.task('parcel-serve', gulpShell.task([
   'npx parcel serve index.html --open',
 ]));
 
-// Default task: run the build task and then serve the project
-gulp.task('default', gulp.series('parcel-build', 'parcel-serve'));
-
-gulp.task('test', () => {
-  return gulp.src('test/shuffle.test.js', { read: false }) 
-    .pipe(mocha({ reporter: 'spec' })) 
-    .on('error', console.error);
-});
+gulp.task('test', gulpShell.task([
+  'npx mocha test/shuffle.test.js --reporter spec'
+]));
 
 gulp.task('cypress', gulpShell.task([
   'npx cypress run'
 ]));
+
+gulp.task('default', gulp.series('parcel-build', 'parcel-serve', 'test'));
+
+
+
