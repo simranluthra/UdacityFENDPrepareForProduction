edb2353e50e23b726b0aa387e4d53c34b2e800e2
simranluthra <simran.19july@gmail.com>
2025-04-26 12:41:50 +0530

    Update shuffle.test.js

--- starter/test/shuffle.test.js
+++ starter/test/shuffle.test.js
@@ -1,15 +1,21 @@
 import { expect } from 'chai';
 import { shuffle } from '../src/shuffle.js';
+import { cardSets } from '../data/data.js';
 
 describe('shuffle function', () => {
-  it('should shuffle the indexes of the array', () => {
-    const originalArray = ['A', 'B', 'C', 'D'];
-    const shuffledArray = shuffle(originalArray);
+  const originalCards = cardSets[0].cards; 
+  const termsOnly = (arr) => arr.map(card => card.term).sort();
 
-    // Check that the shuffled array is not the same as the original array
-    expect(shuffledArray).to.not.eql(originalArray);
-
-    // Check that the shuffled array contains the same elements
-    expect(shuffledArray.sort()).to.eql(originalArray.sort());
+  it('should return a new array of same length', () => {
+    const result = shuffle(originalCards);
+    expect(result).to.be.an('array');
+    expect(result.length).to.equal(originalCards.length);
   });
+
+  it('should contain all the same cards (by term)', () => {
+    const result = shuffle(originalCards);
+    expect(termsOnly(result)).to.eql(termsOnly(originalCards));
+  });
+
+
 });
